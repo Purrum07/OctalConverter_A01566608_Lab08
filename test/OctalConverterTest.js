9fcfedcda57ae471aa836197a746151fc73fb298
Mario Márquez <48485832+Purrum07@users.noreply.github.com>
2021-04-03 22:42:28 -0700

    Challenge 2

--- test/OctalConverterTest.js
+++ test/OctalConverterTest.js
@@ -12,3 +12,15 @@ describe('givenDecimal1WhenConvertThenOctal1', function () {
         assert.strictEqual(expectedResult, actualResult);
     })
 })
+
+describe('givenDecimal2WhenConvertThenOctal2', function () {
+    it('convert(2) = 2', function() {
+        //Given
+        var a = 2;
+        var expectedResult = 2;
+        // When
+        var actualResult = octalConverter.convert(a);
+        // Then
+        assert.strictEqual(expectedResult, actualResult);
+    })
+})
